22666450a386b71b3fbba805c3f76fadbe67b186
Chivon <chhaichivon@2ntkh.com>
2017-05-22 17:07:18 +0700

    create ui

---
+++ src/main/java/com/phsarphnompenh/ui/PhsarphnompenhUiJavaApplication.java
@@ -0,0 +1,12 @@
+package com.phsarphnompenh.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhsarphnompenhUiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhsarphnompenhUiJavaApplication.class, args);
+	}
+}
